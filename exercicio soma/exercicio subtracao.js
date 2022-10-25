1976be6841e498959bffd8fa0f8d078f5c500dbc
isaque rodrigues <isaquepuerodrigues@gmail.com>
2022-10-25 12:56:44 -0300

    exercicio subtracao

---
+++ exercicio soma/exercicio subtracao.js
@@ -0,0 +1,12 @@
+//Você deve criar um programa que solicite ao seu usuário dois
+//números A e B.
+//Crie uma função que receba os dois valores fornecidos pelo
+//usuário e faça uma subtração com eles, e retorne o resultado;
+
+var numeroA = parseInt(prompt())
+var numeroB= parseInt(prompt())
+function subitrairmedia(numeroA, numeroB){
+  console.log(" a subitraçao  da media é ")
+console.log( numeroA + numeroB)
+}
+subitrairmedia(numeroA,numeroB)  
