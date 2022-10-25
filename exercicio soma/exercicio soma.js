72f3547abd0a08edbff9fc33216b0fcd680e628d
isaque rodrigues <isaquepuerodrigues@gmail.com>
2022-10-25 11:57:15 -0300

    exercicio soma

---
+++ exercicio soma/exercicio soma.js
@@ -0,0 +1,13 @@
+//Você deve criar um programa que solicite ao seu usuário dois
+//números A e B.
+//Crie uma função que receba os dois valores fornecidos pelo
+//usuário e faça uma soma com eles, e retorne o resultado
+
+
+var numeroA = parseInt(prompt())
+var numeroB= parseInt(prompt())
+function somarmedia(numeroA, numeroB){
+  console.log(" a soma da media é ")
+console.log( numeroA + numeroB)
+}
+somarmedia(numeroA,numeroB)
