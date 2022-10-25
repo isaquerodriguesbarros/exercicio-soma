bc105c2efa17e87bc802ebf5afc3c904a51331cb
isaque rodrigues <isaquepuerodrigues@gmail.com>
2022-10-25 13:59:18 -0300

    multiplicaçao

---
+++ exercicio soma/multiplicacao.js
@@ -0,0 +1,14 @@
+
+//Crie uma função que receba os dois valores fornecidos pelo
+//usuário e faça uma multiplicação com eles, e retorne o
+//resultado;
+//Crie uma função que receba os dois valores fornecidos pelo
+//usuário e faça uma subtração com eles, e retorne o resultado;
+
+var numeroA = parseInt(prompt())
+var numeroB= parseInt(prompt())
+function mutiplicarmedia(numeroA, numeroB){
+  console.log(" a mutiplicaçao  da media é ")  
+console.log( numeroA * numeroB)
+}
+mutiplicarmedia(numeroA,numeroB)  
