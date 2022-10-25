3bcfe5a194aeaa1dbdfbfe06629c43e64de0ff84
isaque rodrigues <isaquepuerodrigues@gmail.com>
2022-10-25 14:07:42 -0300

    divisao

---
+++ exercicio soma/divisao.js
@@ -0,0 +1,12 @@
+//Você deve criar um programa que solicite ao seu usuário dois
+//números A e B.
+//Crie uma função que receba os dois valores fornecidos pelo
+//usuário e faça uma subtração com eles, e retorne o resultado;
+
+var numeroA = parseInt(prompt())
+var numeroB= parseInt(prompt())
+function dividirmedia(numeroA, numeroB){
+  console.log(" a divisao  da media é ")  
+console.log( numeroA / numeroB)
+}
+dividirmedia(numeroA,numeroB)  
